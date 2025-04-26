ae4e44f3d889d057ef73bfb35fa4cbbd7fe79f34
Benjamin Bolte <ben@bolte.cc>
2025-04-26 02:42:08 -0400

    increase a bit

--- benchmark/train.py
+++ benchmark/train.py
@@ -349,8 +349,8 @@ class HumanoidWalkingTask(ksim.PPOTask[Config], Generic[Config]):
     def get_terminations(self, physics_model: ksim.PhysicsModel) -> list[ksim.Termination]:
         return [
             ksim.BadZTermination(unhealthy_z_lower=0.9, unhealthy_z_upper=1.6),
-            ksim.PitchTooGreatTermination(max_pitch=math.radians(10)),
-            ksim.RollTooGreatTermination(max_roll=math.radians(10)),
+            ksim.PitchTooGreatTermination(max_pitch=math.radians(30)),
+            ksim.RollTooGreatTermination(max_roll=math.radians(30)),
             ksim.FastAccelerationTermination(),
             ksim.FarFromOriginTermination(max_dist=10.0),
         ]
